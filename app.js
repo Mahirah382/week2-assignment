b17eb21889f20f463dfd9ed0df6f50069f4a7da8
mahirah maleek <mahirahmaleek123@gmail.com>
2026-01-16 12:11:23 +0000

    make big images fit screen

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const thumbnailContainer = document.querySelector('.thumbnails')
 const bigDisplay = document.getElementById('bigDisplay')
 
 const images = [
-    { src: 'https://images.unsplash.com/photo-1768310512589-5925669f1784?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    { src: 'https://images.unsplash.com/photo-1760236970108-eedb7add75d8?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       alt: 'a historic japanese pagoda',
     },
 
@@ -14,8 +14,8 @@ const images = [
       alt: 'venice buildings and streets',
     },
 
-    { src: 'https://plus.unsplash.com/premium_photo-1675975706513-9daba0ec12a8?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      alt: 'architecture in rome, italy',
+    { src: 'https://images.unsplash.com/photo-1574864745093-5566c5be5855?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      alt: 'pyraminds in Giza, Egypt',
     },
 ]
 
