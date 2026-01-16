de474cb39884485a92682070debd1e71c230dc9c
mahirah maleek <mahirahmaleek123@gmail.com>
2026-01-16 10:09:11 +0000

    add big images

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const thumbnailContainer = document.querySelector('.thumbnails')
+const bigDisplay = document.getElementById('bigDisplay')
 
 const images = [
-    { scr: 'https://images.unsplash.com/photo-1768310512589-5925669f1784?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      alt: 'an image of a historic japanese pagoda',
+    { src: 'https://images.unsplash.com/photo-1768310512589-5925669f1784?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      alt: 'a historic japanese pagoda',
     },
 
-    { scr: 'https://images.unsplash.com/photo-1768215592110-6283f76ac552?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      alt: 'an image of a train and canyons in a desert',
+    { src: 'https://images.unsplash.com/photo-1768215592110-6283f76ac552?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      alt: 'a train and canyons in a desert',
     },
 
-    { scr: 'https://images.unsplash.com/photo-1558271736-cd043ef2e855?q=80&w=1331&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      alt: 'an image of venice buildings and streets',
+    { src: 'https://images.unsplash.com/photo-1558271736-cd043ef2e855?q=80&w=1331&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      alt: 'venice buildings and streets',
     },
 
-    { scr: 'https://plus.unsplash.com/premium_photo-1675975706513-9daba0ec12a8?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      alt: 'an image of architecture in rome, italy',
+    { src: 'https://plus.unsplash.com/premium_photo-1675975706513-9daba0ec12a8?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      alt: 'architecture in rome, italy',
     },
 ]
 
@@ -25,6 +26,24 @@ function createThumbnails() {
         imageElement.src = image.src
         imageElement.alt = image.alt
 
+        imageElement.addEventListener('click', function () {
+          console.log(image)
+          createBigImage(image)
+        })
+
         thumbnailContainer.append(imageElement)
     })
 }
+
+createThumbnails()
+
+function createBigImage(image) {
+  bigDisplay.innerHTML = ''
+  const bigImage= document.createElement('img');
+
+  bigImage.src = image.src
+  bigImage.alt = image.alt
+
+  bigDisplay.append (bigImage)
+}
+
